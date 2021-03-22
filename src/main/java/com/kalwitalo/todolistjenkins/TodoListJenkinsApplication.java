262a75d5d6e96a5978736b5dc78166b9f42f1683
Kalwitalo <kalwitalo.ibiapina@techlead.com.br>
2021-03-22 20:31:51 -0300

    Initial commit

---
+++ src/main/java/com/kalwitalo/todolistjenkins/TodoListJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.kalwitalo.todolistjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalwitalo/todolistjenkins/TodoListJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalwitalo.todolistjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
